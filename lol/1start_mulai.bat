dedb4e34f5fbf69660dc81463fb7766e06e4e505
nids4easy <nids4easy@gmail.com>
2021-04-30 07:11:36 +0700

    first commit

---
+++ lol/1start_mulai.bat
@@ -0,0 +1,6 @@
+@echo off
+
+lolMiner.exe --algo ETHASH --pool ethash.unmineable.com:3333 --user USDT:0x506fc50f9c916849f80474c9cf677455ef7cff65.oncom1 --ethstratum ETHPROXY
+
+pause
+
---
+++ xla/start-miner.cmd
@@ -0,0 +1,3 @@
+cd %~dp0 
+xlarig.exe -o sg.scala.herominers.com:10130 -p oncomwin -u SvkKp92SYoy8GZHn3LaKoGg8SSQfVGhsfJUumopjGbiLAfkrtCbnQ7JREF4BawqkkxF67JicmbecqDiV4jfTLktd2yG9WLoyb --donate-level 0 -a panthera -k -t 1
+pause 
